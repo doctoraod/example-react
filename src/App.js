2ed0ff1183135426d4dada1949918f6ac0f74400
Suvichan Prasongphonchai <suvichan_pra@sendit.asia>
2017-08-02 15:05:29 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import logo from './logo.svg';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        test
+      </div>
+    );
+  }
+}
+
+export default App;
