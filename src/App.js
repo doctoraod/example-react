e5ee5d2a4d7300419138cdfa23503da7f95afff5
Suvichan Prasongphonchai <suvichan_pra@sendit.asia>
2017-08-02 15:37:02 +0700

    Example fixed-data-table - sort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,151 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import moment from 'moment'
+import FixedDataTable from 'fixed-data-table'
+import 'fixed-data-table/dist/fixed-data-table.min.css'
 
+import FakeObjectDataListStore from './lib/FakeObjectDataListStore'
+import TextCell from './components/TextCell'
+import SortHeaderCell from './components/SortHeaderCell'
+import SortTypes from './constant/SortTypes'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      
-    }
+const { Table, Column } = FixedDataTable;
+
+class DataListWrapper {
+  constructor(indexMap, data) {
+    this._indexMap = indexMap;
+    this._data = data;
   }
-  
-  render() {
-    return (
-      <div className="App">
-        test
-      </div>
+
+  getSize() {
+    return this._indexMap.length;
+  }
+
+  getObjectAt(index) {
+    return this._data.getObjectAt(
+      this._indexMap[index],
     );
   }
 }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this._dataList = new FakeObjectDataListStore(2000);
+
+    this._defaultSortIndexes = [];
+    var size = this._dataList.getSize();
+    for (var index = 0; index < size; index++) {
+      this._defaultSortIndexes.push(index);
+    }
+
+    this.state = {
+      sortedDataList: this._dataList,
+      colSortDirs: {},
+    };
+
+    this._onSortChange = this._onSortChange.bind(this);
+  }
+  _onSortChange(columnKey, sortDir) {
+    var sortIndexes = this._defaultSortIndexes.slice();
+    sortIndexes.sort((indexA, indexB) => {
+      var valueA = this._dataList.getObjectAt(indexA)[columnKey];
+      var valueB = this._dataList.getObjectAt(indexB)[columnKey];
+      var sortVal = 0;
+      if (valueA > valueB) {
+        sortVal = 1;
+      }
+      if (valueA < valueB) {
+        sortVal = -1;
+      }
+      if (sortVal !== 0 && sortDir === SortTypes.ASC) {
+        sortVal = sortVal * -1;
+      }
+
+      return sortVal;
+    });
+
+    this.setState({
+      sortedDataList: new DataListWrapper(sortIndexes, this._dataList),
+      colSortDirs: {
+        [columnKey]: sortDir,
+      },
+    });
+  }
+  render() {
+    var { sortedDataList, colSortDirs } = this.state;
+    return (
+      <Table
+        rowHeight={50}
+        rowsCount={sortedDataList.getSize()}
+        headerHeight={50}
+        width={1000}
+        height={500}
+        {...this.props}>
+        <Column
+          columnKey="id"
+          header={
+            <SortHeaderCell
+              onSortChange={this._onSortChange}
+              sortDir={colSortDirs.id}>
+              id
+            </SortHeaderCell>
+          }
+          cell={<TextCell data={sortedDataList} />}
+          width={100}
+        />
+        <Column
+          columnKey="firstName"
+          header={
+            <SortHeaderCell
+              onSortChange={this._onSortChange}
+              sortDir={colSortDirs.firstName}>
+              First Name
+            </SortHeaderCell>
+          }
+          cell={<TextCell data={sortedDataList} />}
+          width={200}
+        />
+        <Column
+          columnKey="lastName"
+          header={
+            <SortHeaderCell
+              onSortChange={this._onSortChange}
+              sortDir={colSortDirs.lastName}>
+              Last Name
+            </SortHeaderCell>
+          }
+          cell={<TextCell data={sortedDataList} />}
+          width={200}
+        />
+        <Column
+          columnKey="city"
+          header={
+            <SortHeaderCell
+              onSortChange={this._onSortChange}
+              sortDir={colSortDirs.city}>
+              City
+            </SortHeaderCell>
+          }
+          cell={<TextCell data={sortedDataList} />}
+          width={200}
+        />
+        <Column
+          columnKey="companyName"
+          header={
+            <SortHeaderCell
+              onSortChange={this._onSortChange}
+              sortDir={colSortDirs.companyName}>
+              Company Name
+            </SortHeaderCell>
+          }
+          cell={<TextCell data={sortedDataList} />}
+          width={200}
+        />
+      </Table>
+    );
+  }
+}
+
 
 export default App;
---
+++ src/components/SortHeaderCell.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Cell } from 'fixed-data-table'
+import SortTypes from '../constant/SortTypes'
+
+function reverseSortDirection(sortDir) {
+  return sortDir === SortTypes.DESC ? SortTypes.ASC : SortTypes.DESC;
+}
+
+class SortHeaderCell extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onSortChange = this._onSortChange.bind(this);
+  }
+
+  render() {
+    var {sortDir, children, ...props} = this.props;
+    return (
+      <Cell {...props}>
+        <a onClick={this._onSortChange}>
+          {children} {sortDir ? (sortDir === SortTypes.DESC ? '↓' : '↑') : ''}
+        </a>
+      </Cell>
+    );
+  }
+
+  _onSortChange(e) {
+    e.preventDefault();
+
+    if (this.props.onSortChange) {
+      this.props.onSortChange(
+        this.props.columnKey,
+        this.props.sortDir ?
+          reverseSortDirection(this.props.sortDir) :
+          SortTypes.DESC
+      );
+    }
+  }
+}
+
+export default SortHeaderCell
